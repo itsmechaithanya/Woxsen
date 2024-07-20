2f5d71f0c62a813ecd559d49cdea3f042bc11113
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-07-20 19:04:44 +0530

    first commit

--- script.js
+++ script.js
@@ -391,6 +391,52 @@ function swiper(){
 }
 swiper();
 
+var active = 5;
+var mncircles = document.querySelectorAll(".mncircle");
+var sec = document.querySelectorAll(".second");
+
+gsap.to(mncircles[active-1],{
+    opacity: .5,
+
+})
+gsap.to(sec[active-1],{
+    opacity: 1,
+
+})
+
+mncircles.forEach(function(val, index) {
+    val.addEventListener("click", function() {
+        gsap.to("#circle", {
+            rotate: (5 - (index + 1)) * 10,
+            ease: Expo.easeInOut,
+            duration: 1,
+        });
+        gray();
+        gsap.to(this,{
+            opacity: .5,
+        })
+        gsap.to(sec[index],{
+            opacity: 1,
+        })
+    });
+});
+
+function gray(){
+    gsap.to(mncircles, {
+        opacity: .08,
+    })
+    gsap.to(sec, {
+        opacity: .4,
+    })
+}
+
+gsap.to("#circle",{
+    rotate:0,
+    ease:Expo.easeInOut,
+    duration: 2,
+})
+
+
 Shery.makeMagnet(".magnet");
 
 // gsap.to(".elem",{
